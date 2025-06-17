4b19feb343b09db4a2b3637e6d16f77f823ef107
Arvydas <106098508+ArvydasBeg@users.noreply.github.com>
2025-06-17 17:00:56 +0000

    Add files via upload
    cors fixing
    
    Signed-off-by: Arvydas <106098508+ArvydasBeg@users.noreply.github.com>

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://evhub.space", "https://www.evhub.space"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 
 app.use(express.static("public"));
